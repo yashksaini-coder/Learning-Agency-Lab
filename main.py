a335d1524da752c15a9afbee31a86c1b9e6e0414
yashksaini-coder <ys3853428@gmail.com>
2024-05-10 01:08:50 +0530

    Load Train & Test data

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from src.load import load_train
+# Importing the basic libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-load_train()
+
+from src.load import load_train, load_test, load_submission
+
+train = load_train()
+test = load_test()
+

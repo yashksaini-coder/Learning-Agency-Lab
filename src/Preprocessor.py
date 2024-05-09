f02e359ec9614e3ebfcda33739821b31a8b226f5
yashksaini-coder <ys3853428@gmail.com>
2024-05-10 01:48:45 +0530

    Used 'tqdm' class for applying Spelling data conversion

--- src/Preprocessor.py
+++ src/Preprocessor.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from tqdm import tqdm
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.tokenize.treebank import TreebankWordDetokenizer
@@ -22,35 +22,35 @@ class Preprocessor:
         data["text_length"] = data["full_text"].apply(lambda x: len(x))
         data["word_count"] = data["text_tokens"].apply(lambda x: len(x))
         data["unique_word_count"] = data["text_tokens"].apply(lambda x: len(set(x)))
-        data["splling_err_num"] = data["full_text"].progress_apply(self.spelling)
-    
+        data["splling_err_num"] = tqdm(data["full_text"].apply(self.spelling))
+   
         data["processed_text"] = data["full_text"].apply(lambda x: dataPreprocessing(x))
         data["text_tokens"] = data["processed_text"].apply(lambda x: word_tokenize(x))
         data["text_length_p"] = data["processed_text"].apply(lambda x: len(x))
         data["word_count_p"] = data["text_tokens"].apply(lambda x: len(x))
         data["unique_word_count_p"] = data["text_tokens"].apply(lambda x: len(set(x)))
-        data["splling_err_num_p"] = data["processed_text"].progress_apply(self.spelling)
+        data["splling_err_num_p"] = tqdm(data["processed_text"].apply(self.spelling))
     
         data["processed_text"] = data["full_text"].apply(lambda x: dataPreprocessing_w_contract(x))
         data["text_tokens"] = data["processed_text"].apply(lambda x: word_tokenize(x))
         data["text_length_pc"] = data["processed_text"].apply(lambda x: len(x))
         data["word_count_pc"] = data["text_tokens"].apply(lambda x: len(x))
         data["unique_word_count_pc"] = data["text_tokens"].apply(lambda x: len(set(x)))
-        data["splling_err_num_pc"] = data["processed_text"].progress_apply(self.spelling)
+        data["splling_err_num_pc"] = tqdm(data["processed_text"].apply(self.spelling))
         
         data["processed_text"] = data["full_text"].apply(lambda x: dataPreprocessing_w_punct_remove(x))
         data["text_tokens"] = data["processed_text"].apply(lambda x: word_tokenize(x))
         data["text_length_ppr"] = data["processed_text"].apply(lambda x: len(x))
         data["word_count_ppr"] = data["text_tokens"].apply(lambda x: len(x))
         data["unique_word_count_ppr"] = data["text_tokens"].apply(lambda x: len(set(x)))
-        data["splling_err_num_ppr"] = data["processed_text"].progress_apply(self.spelling)
+        data["splling_err_num_ppr"] = tqdm(data["processed_text"].apply(self.spelling))
         
         data["processed_text"] = data["full_text"].apply(lambda x: dataPreprocessing_w_contract_punct_remove(x))
         data["text_tokens"] = data["processed_text"].apply(lambda x: word_tokenize(x))
         data["text_length_pcpr"] = data["processed_text"].apply(lambda x: len(x))
         data["word_count_pcpr"] = data["text_tokens"].apply(lambda x: len(x))
         data["unique_word_count_pcpr"] = data["text_tokens"].apply(lambda x: len(set(x)))
-        data["splling_err_num_pcpr"] = data["processed_text"].progress_apply(self.spelling)
+        data["splling_err_num_pcpr"] = tqdm(data["processed_text"].apply(self.spelling))
         data.drop(columns=["processed_text", "text_tokens"], inplace=True)
         return data
     

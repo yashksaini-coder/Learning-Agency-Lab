6b6c8dfa433881966e4f0cc01d8d9275fe931cfc
yashksaini-coder <ys3853428@gmail.com>
2024-05-12 17:11:09 +0530

    Create Vectorization.py

---
+++ src/Vectorization.py
@@ -0,0 +1,3 @@
+"""This scripts is used to perform the process of text vectorization using TF-IDF (Term Frequency-Inverse Document Frequency),
+and then merges the TF-IDF features with the previously generated features from paragraphs and sentences.
+"""
